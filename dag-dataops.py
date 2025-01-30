9028ccb6d1bafdfc2ddab3a014dc8a29fb1ded64
rutube-2022 <119008582+rutube-2022@users.noreply.github.com>
2025-01-30 20:22:44 +0300

    Create dag-dataops.py

---
+++ dag-dataops.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+#import sqlalchemy
+import psycopg2
+  
+dag = DAG ('tusur-exam')
+
+def check():
+    con = psycopg2.connect(host="psql-postgresql.psql.svc",
+                           database="airflow",
+                           user="airflow",
+                           password="airflow")
+    nncur = con.cursor()
+    nncur.execute ("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+    nncur.close()
+    con.close()
+check_task = PythonOperator(
+    task_id="check_task",
+    python_callable=check,
+    dag=dag,
+)
+
